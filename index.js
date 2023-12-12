2ae759e0039bb1f93aa3fb9ae4d7bed661d7b06a
LalitDeore30 <115993677+LalitDeore30@users.noreply.github.com>
2023-12-12 14:38:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var type = new Typed(".multiple-text",{
+    strings:["Frontend Developer","Backend Developer","Fullstack Developer","Coder"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+})
+      function openNav() {
+        document.querySelector('.navbar').style.width = '250px';
+      }
+
+      function closeNav() {
+        document.querySelector('.navbar').style.width = '0';
+      }
