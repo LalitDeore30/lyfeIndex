779a0c675f497409a57089e2311ece2cbaf9ce8c
LalitDeore30 <115993677+LalitDeore30@users.noreply.github.com>
2023-12-14 22:49:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-var type = new Typed(".multiple-text",{
-    strings:["Frontend Developer","Backend Developer","Fullstack Developer","Coder"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true,
-})
-import { Collapse, Ripple, initMDB } from "mdb-ui-kit";
 
-initMDB({ Collapse, Ripple });
